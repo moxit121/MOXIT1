5160a1399063779c6a27a3eda5bc7810339875c4
Moxit Shah <moxitshah121@gmail.com>
2022-12-23 20:14:50 +0530

    Create intro.c

---
+++ C PROGRAMMING/CLASSWORK/21-12-22/intro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main ()
+{
+	printf("MY NAME IS MOXIT\n");
+	printf("BCA - SOMLALIT INSTITUTE OF COMPUTER APPLICATIONS\n");
+	printf("---------------------------------------------------");
+}
